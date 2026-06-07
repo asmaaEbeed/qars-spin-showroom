7e2d9dc9d809443da28afc7cba796a519b2b7965
asmaaEbeed <36919956+asmaaEbeed@users.noreply.github.com>
2026-06-07 11:12:13 +0300

    Fix/Previous issues

---
+++ src/components/posts/constants/post-constants.jsx
@@ -0,0 +1,13 @@
+export const QARS_SPIN_PARTNER_ID = 59;
+
+
+
+export const POST_STATUS = Object.freeze({
+  APPROVED: "Approved",
+  PENDING_APPROVAL: "Pending Approval",
+  REJECTED: "Rejected",
+  REJECTED_PERMANENTLY: "Rejected Permanently",
+  SUSPENDED: "Suspended",
+  SUSPENDED_PERMANENTLY: "Suspended Permanently",
+  DRAFT: "Draft",
+});
